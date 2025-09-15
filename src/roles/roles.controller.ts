f40d560c9055ae36106d5b832ef4df847a6fcae3
Leydi22AD <leydi.arevalo@upeu.edu.pe>
2025-09-15 18:51:32 -0500

    Actualización del proyecto en clase-2

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,34 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { RolesService } from './roles.service';
-import { CreateRoleDto } from './dto/create-rol.dto';
-import { UpdateRoleDto } from './dto/update-role.dto';
+import { CreateRolDto } from './dto/create-rol.dto';
+
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.rolesService.create(createRoleDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.rolesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  create(@Body() rol: CreateRolDto) {
+    return this.rolesService.create(rol);
   }
 }
--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rol } from './roles.entity';
+import { User } from 'src/users/users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Rol,User])],
   controllers: [RolesController],
   providers: [RolesService],
 })
